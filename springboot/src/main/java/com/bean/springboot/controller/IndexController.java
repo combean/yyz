4c64eaaee0b66191d18d1e00924cc8dab98f3283
wodeln <lina521521>
2017-07-11 08:42:53 +0800

    session

--- springboot/src/main/java/com/bean/springboot/controller/IndexController.java
+++ springboot/src/main/java/com/bean/springboot/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.bean.springboot.controller;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,16 @@ import javax.servlet.http.HttpServletResponse;
  * Created by ppctest02 on 2017/3/21.
  */
 @Controller
+@RequestMapping("/index")
 public class IndexController {
 
     @Value(value="${lyw.name}")
     private String a;
     @RequestMapping("/test")
-    public String test(HttpServletResponse response) {
-//            try {
+    public String test(HttpServletResponse response, HttpServletRequest request) {
+        request.getSession().setAttribute("userId",1);
+        request.getSession().setAttribute("userName","赵岩");
+        //            try {
 ////                vendingService.getVendingAdvByScreenAndToken(new SimpleDateFormat("yyyyMMddHHmmss").parse("20160708153908"),"1");
 //            } catch (ParseException e) {
 //                e.printStackTrace();
